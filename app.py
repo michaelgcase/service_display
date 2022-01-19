6f8e03b79b475e7724d06ad080cc91e781b23121
michaelgcase <michaelgordoncase@gmail.com>
2022-01-19 23:21:00 +0000

    app change

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import ploty.express as px
+import pandas as pd
 
-@app.route('/')
-def hello_world():
-	return "Hello World"
+app = dash.DASH(__name__)
+df = pd.DataFrame({
+	"Fruit" : ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+	"Amount" : [4, 1, 2, 2, 4, 5],
+	"City" : ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmore="group")
+app.layout = html.Div(children=[
+	html.H1(children="Hello Dash"),
+
+	html.Div(children='''
+		Dash: A web application framework for you data
+	'''),
+
+	dcc.Graph(
+		id='example-graph',
+		figure=fig
+	)
+])
+
+if __name__ == '__main__':
+	app.run_server(debug=True)
+
 
 
